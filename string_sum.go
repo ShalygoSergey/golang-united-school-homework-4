39953d4a209db751498ca7fdb605d8fca1cce59b
Шалыго Сергей Михайлович <Shalygo.S.Mi@sberbank.ru>
2022-05-15 11:28:01 +0300

    первая попытка

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,65 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	fields := strings.Fields(input)
+
+	if len(fields) == 0 {
+		return "", fmt.Errorf("StringSum: %w", errorEmptyInput)
+	}
+
+	in := strings.Join(fields, "")
+
+	//m := regexp.MustCompile(`-?\+?\[0-9\]-?\+?`)
+	//if len(m.ReplaceAllString(in, "")) != 0 {
+	//	return "", fmt.Errorf("StringSum: %w", ?)
+	//}
+
+	op := make([]string, 0)
+	tmp := ""
+	runes := []rune(in)
+	for i := 0; i < len(runes); i = i + 1 {
+		if string(runes[i]) == "+" || string(runes[i]) == "-" {
+			if len(tmp) != 0 {
+				op = append(op, tmp)
+				tmp = ""
+			}
+			op = append(op, string(runes[i]))
+		} else {
+			tmp = tmp + string(runes[i])
+		}
+	}
+	if len(tmp) != 0 {
+		op = append(op, tmp)
+	}
+
+	count := 0
+	operator := "+"
+	value := 0
+	for i := 0; i < len(op); i = i + 1 {
+
+		if op[i] == "+" {
+			operator = "+"
+		} else if op[i] == "-" {
+			operator = "-"
+		} else {
+			val, err := strconv.Atoi(op[i])
+			if err != nil {
+				return "", fmt.Errorf("StringSum: %w", err)
+			}
+
+			if operator == "+" {
+				value = value + val
+			} else {
+				value = value - val
+			}
+			count = count + 1
+		}
+	}
+
+	if count != 2 {
+		return "", fmt.Errorf("StringSum: %w", errorNotTwoOperands)
+	}
+
+	return strconv.Itoa(value), nil
 }
